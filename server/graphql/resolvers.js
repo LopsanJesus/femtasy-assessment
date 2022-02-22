985868262e6d7eb98a5216448bd1bc9b00925f12
Jesus L <lopsanjesus@gmail.com>
2022-02-22 04:14:02 +0100

    Change resolver for return to call the API

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,45 +1,18 @@
 const { swapi } = require("../apiLinks");
 const { getFilms, getFilm, getCharacter } = require("./helpers");
-const { mockedFilms, mockedCharacters } = require("./../swapiMocker");
 
-// Mock resolvers
 const resolvers = {
     Query: {
-        films: () => {
-            return mockedFilms.map((film) => {
-                return {
-                    ...film,
-                    director: null,
-                    description: null,
-                    characters: null,
-                };
-            });
+        films: async () => {
+            return getFilms(swapi.films);
         },
         film: async (_, { id }) => {
-            return mockedFilms.find((film) => {
-                return film.id == id;
-            });
+            return getFilm(swapi.films + id);
         },
         character: async (_, { id }) => {
-            return mockedCharacters.find((character) => {
-                return character.id == id;
-            });
+            return getCharacter(swapi.characters + id);
         },
     },
 };
-
-// const resolvers = {
-//     Query: {
-//         films: async () => {
-//             return getFilms(swapi.films);
-//         },
-//         film: async (_, { id }) => {
-//             return getFilm(swapi.films + id);
-//         },
-//         character: async (_, { id }) => {
-//             return getCharacter(swapi.characters + id);
-//         },
-//     },
-// };
 
 module.exports = resolvers;
