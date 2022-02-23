0313df7cce4dda0a98f9792fa60716539c5dda5f
Jesus L <lopsanjesus@gmail.com>
2022-02-23 09:30:49 +0100

    Add GraphQL tests

--- server/index.test.js
+++ server/index.test.js
@@ -1,37 +1,38 @@
 const server = require("./server");
+describe("graphql tests", () => {
+    it("lists the films from Swapi", async () => {
+        const result = await server.executeOperation({
+            query: "{ films { title cover characters { name }} }",
+        });
 
-it("lists the films from Swapi", async () => {
-    const result = await server.executeOperation({
-        query: "{ films { title cover characters { name }} }",
+        expect(result.errors).toBeUndefined();
+        expect(result.data?.films[0].title).toBe("A New Hope");
+        expect(result.data?.films[0].characters).toBeNull();
     });
 
-    expect(result.errors).toBeUndefined();
-    expect(result.data?.films[0].title).toBe("A New Hope");
-    expect(result.data?.films[0].characters).toBeNull();
-});
+    it("finds a film in Swapi", async () => {
+        const result = await server.executeOperation({
+            query: "{ film(id: 4) { title director cover characters { name }} }",
+        });
 
-it("finds a film in Swapi", async () => {
-    const result = await server.executeOperation({
-        query: "{ film(id: 4) { title director cover characters { name }} }",
+        expect(result.errors).toBeUndefined();
+        expect(result.data?.film.title).toBe("The Phantom Menace");
+        expect(result.data?.film.director).toBe("George Lucas");
+        expect(result.data?.film.cover).toBe(
+            "https://starwars-visualguide.com/assets/img/films/4.jpg"
+        );
+        expect(result.data?.film.characters[0].name).toBe("C-3PO");
     });
 
-    expect(result.errors).toBeUndefined();
-    expect(result.data?.film.title).toBe("The Phantom Menace");
-    expect(result.data?.film.director).toBe("George Lucas");
-    expect(result.data?.film.cover).toBe(
-        "https://starwars-visualguide.com/assets/img/films/4.jpg"
-    );
-    // expect(result.data?.film.characters[0].name).toBe("C-3PO");
-});
+    it("finds a character in Swapi", async () => {
+        const result = await server.executeOperation({
+            query: "{ character(id: 1) { name picture} }",
+        });
 
-it("finds a character in Swapi", async () => {
-    const result = await server.executeOperation({
-        query: "{ character(id: 1) { name picture} }",
+        expect(result.errors).toBeUndefined();
+        expect(result.data?.character.name).toBe("Luke Skywalker");
+        expect(result.data?.character.picture).toBe(
+            "https://starwars-visualguide.com/assets/img/characters/1.jpg"
+        );
     });
-
-    expect(result.errors).toBeUndefined();
-    expect(result.data?.character.name).toBe("Luke Skywalker");
-    expect(result.data?.character.picture).toBe(
-        "https://starwars-visualguide.com/assets/img/characters/1.jpg"
-    );
 });
