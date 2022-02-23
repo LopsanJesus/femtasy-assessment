e55b2e7fe53230e98eacf1f8859d62e8950e0cbc
Jesus L <lopsanjesus@gmail.com>
2022-02-23 09:06:55 +0100

    Add Character component content

--- frontend/src/components/Character/Character.js
+++ frontend/src/components/Character/Character.js
@@ -8,12 +8,16 @@ import Spinner from "../Spinner/Spinner";
 import FavoriteStar from "../FavoriteStar/FavoriteStar";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import StorageHelper from "../../helpers/storage";
+import Picture from "../Picture/Picture";
 
 const Character = () => {
     let params = useParams();
+    let characterId = !isNaN(parseInt(params.characterId))
+        ? parseInt(params.characterId)
+        : 1;
 
     const { loading, error, data } = useQuery(GET_CHARACTER, {
-        variables: { id: parseInt(params.characterId) },
+        variables: { id: characterId },
     });
 
     const storeName = "characters";
@@ -29,16 +33,50 @@ const Character = () => {
     ) : error ? (
         <ErrorMessage>Server error</ErrorMessage>
     ) : (
-        <div>
-            <FavoriteStar
-                initialValue={StorageHelper.isFavorited(
-                    params.characterId,
-                    storeName
-                )}
-                onFavClick={onFavClick}
-            />
-            <h3>{data.character.name}</h3>
-            <p>{data.character.gender}</p>
+        <div className="Character">
+            <div className="relative z-10 float-right mr-4 md:mr-10">
+                <FavoriteStar
+                    initialValue={StorageHelper.isFavorited(
+                        params.characterId,
+                        storeName
+                    )}
+                    onFavClick={onFavClick}
+                />
+            </div>
+            <h3 className="m-4 text-3xl font-bold film-title drop-shadow-md">
+                {data.character.name}
+            </h3>
+            <div className="md:grid md:grid-cols-2">
+                <div className="my-6 lg:px-14">
+                    <Picture src={data.character.picture} size="big" />
+                </div>
+                <div className="leading-10 md:mt-10 md:text-3xl">
+                    <p>
+                        <span className="font-bold">Gender: </span>
+                        {data.character.gender}
+                    </p>
+                    <p>
+                        <span className="font-bold">Homeworld: </span>
+                        {data.character.homeworld}
+                    </p>
+                    <div className="mt-4">
+                        <p className="font-bold">Description:</p>
+                        <p>
+                            Birth year: {data.character.description.birthYear}
+                        </p>
+                        <p>Height: {data.character.description.height}cm</p>
+                        <p>Weight: {data.character.description.mass}kg</p>
+                        <p>
+                            Skin color: {data.character.description.skinColor}
+                        </p>
+                        <p>
+                            Hair color: {data.character.description.hairColor}
+                        </p>
+                        <p>Eye color: {data.character.description.eyeColor}</p>
+                    </div>
+                </div>
+            </div>
+
             <BackButton />
         </div>
     );
--- frontend/src/components/Character/Character.test.js
+++ frontend/src/components/Character/Character.test.js
@@ -1,10 +1,103 @@
 import React from "react";
-import { shallow } from "enzyme";
 import Character from "./Character";
+import { MockedProvider } from "@apollo/client/testing";
+import { GET_CHARACTER } from "../../queries/characters";
+import TestRenderer from "react-test-renderer";
+import Picture from "../Picture/Picture";
+const { act } = TestRenderer;
+
+jest.mock("../BackButton/BackButton", () => () => {
+    const BackButton = "default-awesome-component-mock";
+    return <BackButton />;
+});
+
+const mockCharacter = {
+    character: {
+        id: 1,
+        name: "Luke Skywalker",
+        gender: "male",
+        homeworld: "Tatooine",
+        picture: "https://starwars-visualguide.com/assets/img/characters/1.jpg",
+        description: {
+            birthYear: "19BBY",
+            height: "172",
+            mass: "77",
+            skinColor: "fair",
+            hairColor: "blond",
+            eyeColor: "blue",
+        },
+    },
+};
+
+const errorMocks = [
+    {
+        request: {
+            query: GET_CHARACTER,
+            variables: {
+                id: "abc",
+            },
+        },
+        error: new Error("An error occurred"),
+    },
+];
+
+const mocks = [
+    {
+        request: {
+            query: GET_CHARACTER,
+            variables: {
+                id: 1,
+            },
+        },
+        result: {
+            data: mockCharacter,
+        },
+    },
+];
 
 describe("Character", () => {
-    test("matches snapshot", () => {
-        // const wrapper = shallow(<Character />);
-        // expect(wrapper).toMatchSnapshot();
+    it("shows error message", async () => {
+        const component = TestRenderer.create(
+            <MockedProvider mocks={errorMocks} addTypename={false}>
+                <Character />
+            </MockedProvider>
+        );
+
+        await act(async () => {
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+        });
+
+        const tree = component.toJSON();
+        expect(tree.children.includes("Server error")).toBe(true);
+    });
+
+    it("renders one character", async () => {
+        const component = TestRenderer.create(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <Character />
+            </MockedProvider>
+        );
+
+        await act(async () => {
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+        });
+
+        const h3 = component.root.findByType("h3");
+        expect(h3.children.join("")).toContain("Luke Skywalker");
+    });
+
+    it("renders the film picture", async () => {
+        const component = TestRenderer.create(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <Character />
+            </MockedProvider>
+        );
+
+        await act(async () => {
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+        });
+
+        const img = component.root.findByType(Picture).findByType("img");
+        expect(img.props.className.includes("Picture-img")).toBe(true);
     });
 });
